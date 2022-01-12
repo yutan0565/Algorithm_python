ed984ff97852d06273aaaf48025933f2cc7d5d96
KimYutan <yutan056575@gmail.com>
2022-01-12 12:32:42 +0900

    07_문자열

--- BaekJoon_SBS/07_문자열/알파벳_찾기.py
+++ BaekJoon_SBS/07_문자열/알파벳_찾기.py
@@ -1,14 +1,20 @@
 import sys
 
 s = list(sys.stdin.readline().strip())
-print(s)
-
 al_list = [chr(i) for i in range(97,123)]
-print(al_list)
-
+n = 0
 for i in al_list:
     if i in s:
-        i = in
-
+        index = 0
+        for j in range(len(s)):
+            if i == s[j]:
+                index = j
+                break
+        al_list[n] = index
+    else:
+        al_list[n] = -1
+    n += 1
+for i in al_list:
+    print(i, end=" ")
 
 
