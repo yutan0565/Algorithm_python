6b6c0633c46f744bea3c25d3d7e7c08573c7d328
KimYutan <yutan056575@gmail.com>
2022-01-26 01:25:13 +0900

    01_입출력과사칙연산

---
+++ BaekJoon_SBS/01_입출력과_사칙연산/1998년생인 내가 태국에서는 2541년생.py
@@ -0,0 +1,8 @@
+import sys
+
+
+year = int(sys.stdin.readline())
+
+new_year = year - 543
+
+print(new_year)
---
+++ BaekJoon_SBS/01_입출력과_사칙연산/물음물음느낌표.py
@@ -0,0 +1,5 @@
+import sys
+
+s = sys.stdin.readline().rstrip()
+
+print("{}??!".format(s))
