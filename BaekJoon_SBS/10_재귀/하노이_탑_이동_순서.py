c96fb6466c9b5d9f9fad5fd366adfc9f7cd91647
KimYutan <yutan056575@gmail.com>
2022-02-27 00:39:45 +0900

    BaekJoon

---
+++ BaekJoon_SBS/10_재귀/하노이_탑_이동_순서.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def hanoi(n, start, end):
+    if n == 1:
+        print(start, end)
+        return
+
+    hanoi(n - 1, start, 6 - start - end)
+    print(start, end)
+    hanoi(n - 1, 6 - start - end, end)
+
+
+n = int(sys.stdin.readline())
+print(2 ** n - 1)
+hanoi(n, 1, 3)
