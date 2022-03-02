bd3c4c6bad6b84f9a59a2b4c9bd4a05ca94b167a
KimYutan <70746658+yutan0565@users.noreply.github.com>
2022-03-03 00:09:07 +0900

    Create 01_타일.py

---
+++ BaekJoon_SBS/15_동적_계획법_01/01_타일.py
@@ -0,0 +1,12 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+
+tile = [0]* (1000001)
+tile[1] = 1
+tile[2] = 2
+
+for i in range(3, n+1):
+    tile[i] = (tile[i-1] + tile[i-2])%15746
+print(tile[n])
+
