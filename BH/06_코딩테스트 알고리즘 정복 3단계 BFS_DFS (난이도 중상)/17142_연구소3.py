79b59a11577ed3479bcaa6ac5e8b949b428c99b8
KimYutan <70746658+yutan0565@users.noreply.github.com>
2022-08-23 21:37:10 +0900

    Create 17142_연구소3.py

---
+++ BH/06_코딩테스트 알고리즘 정복 3단계 BFS_DFS (난이도 중상)/17142_연구소3.py
@@ -0,0 +1,58 @@
+import sys
+from collections import deque
+from itertools import combinations
+
+def bfs(graph, visited, act_virus, not_virus):
+    q = deque()
+    max_time = 0
+    for v in act_virus:
+        q.append(v)
+        visited[v[0]][v[1]] = 0
+    dx = [0,0,1,-1]
+    dy = [1,-1,0,0]
+
+    while q:
+        for _ in range(len(q)):
+            a,b = q.popleft()
+            if [a, b] in not_virus:
+                continue
+            else:
+
+                for i in range(4):
+                    nx = a + dx[i]
+                    ny = b + dy[i]
+                    if 0<=nx<n and 0<=ny<n:
+                        if visited[nx][ny] == -1:
+                            if graph[nx][ny] == 0:
+                                q.append([nx,ny])
+                                visited[nx][ny] = visited[a][b] + 1
+                                max_time = max(max_time, visited[nx][ny])
+    for i in range(n):
+        for j in range(n):
+            if visited[i][j] == -1 and graph[i][j] == 0:
+                return -1
+
+    return max_time
+
+n,m = map(int, sys.stdin.readline().rstrip().split())
+
+virus = []
+graph = []
+for i in range(n):
+    graph.append(list(map(int, sys.stdin.readline().rstrip().split())))
+    for j in range(n):
+        if graph[i][j] == 2:
+            virus.append([i,j])
+
+act_virus_group = combinations(virus, m)
+result_list = []
+for act_virus in act_virus_group:
+    visited = [[-1 for _ in range(n)] for _ in range(n)]
+    not_virus = [ v for v in virus if v not in act_virus ]
+    temp = bfs(graph, visited, act_virus, not_virus)
+    result_list.append(temp)
+
+if -1 in result_list:
+    print(-1)
+else:
+    print(min(result_list))
