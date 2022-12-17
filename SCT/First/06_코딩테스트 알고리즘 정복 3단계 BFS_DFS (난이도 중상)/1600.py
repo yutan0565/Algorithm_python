04414efc2dac4094f925e2eb627daee0bb9d6222
KimYutan <70746658+yutan0565@users.noreply.github.com>
2022-12-17 22:24:08 +0900

    Update 1600.py

--- SCT/First/06_코딩테스트 알고리즘 정복 3단계 BFS_DFS (난이도 중상)/1600.py
+++ SCT/First/06_코딩테스트 알고리즘 정복 3단계 BFS_DFS (난이도 중상)/1600.py
@@ -1,10 +1,16 @@
 from collections import deque
 import sys
-
+"""
+1. 특수 이동을 몇번하는가 (벽을 몇번 부수는가)에 따라 결과가 달라지기 때문에, 그에 맞는 visited 생성 !!
+2. 몇번 특수 이동을 하고, 그 자리에서 어떻게 이동하는가는 항상 다르기 때문에,
+- 특수 이동 경우 / 그냥   단순 이분법으로 나누면 답이 틀림 !
+3. 벽을 부수는 거도 똑같음 !! 
+"""
 def bfs():
     q = deque()
-    q.append([start[0], start[1], 0])
-    visited[start[0]][start[1]] = 0
+    count = 0
+    q.append([start[0], start[1],count])
+    visited[start[0]][start[1]][count] = 0
 
     dx_horse = [2,2,-2,-2,1,1,-1,-1]
     dy_horse = [1,-1,1,-1,2,-2,2,-2]
@@ -12,39 +18,41 @@ def bfs():
     dx_mon = [0,0,1,-1]
     dy_mon = [1,-1,0,0]
 
-    dx = dx_horse + dx_mon
-    dy = dy_horse + dy_mon
-
     while q:
-        a,b, count = q.popleft()
+        a,b, cnt= q.popleft()
         if [a,b] == end:
-            return visited[a][b]
-        for i in range(len(dx)):
-            nx = a + dx[i]
-            ny = b + dy[i]
-            if 0<=nx<h and 0<=ny<w:
-                if visited[nx][ny] == -1:
+            return visited[a][b][cnt]
+
+        if cnt < k:
+            for i in range(8):
+                nx = a + dx_horse[i]
+                ny = b + dy_horse[i]
+                if 0<=nx<h and 0<=ny<w :
+                    if visited[nx][ny][cnt+1] == -1:
+                        if graph[nx][ny] == 0:
+                            q.append([nx, ny, cnt + 1])
+                            visited[nx][ny][cnt+1] = visited[a][b][cnt] + 1
+
+        for i in range(4):
+            nx = a + dx_mon[i]
+            ny = b + dy_mon[i]
+            if 0<=nx<h and 0<=ny<w :
+                if visited[nx][ny][cnt] == -1:
                     if graph[nx][ny] == 0:
-                        if count < k and 0<=i<8: #말처럼
-                                print(count)
-                                q.append([nx,ny, count + 1])
-                                visited[nx][ny] = visited[a][b] + 1
-                        else: #원숭이
-                            q.append([nx,ny, count])
-                            visited[nx][ny] = visited[a][b] + 1
+                        q.append([nx,ny, cnt])
+                        visited[nx][ny][cnt] = visited[a][b][cnt] +1
     return -1
 
 
 k = int(sys.stdin.readline().rstrip())
 w,h = map(int, sys.stdin.readline().rstrip().split())
 graph = [list(map(int, sys.stdin.readline().rstrip().split())) for _ in range(h)]
-visited = [ [-1 for _ in range(w)] for _ in range(h)]
+visited = [ [[-1 for _ in range(k+1)] for _ in range(w)] for _ in range(h)]
 
 start = [0,0]
 end = [h-1, w-1]
 
 result = bfs()
-for v in visited:
-    print(v)
-
 print(result)
+
+
