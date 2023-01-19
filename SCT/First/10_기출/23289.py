6716109b671526cdc76f69ed6fada80e5d3998d1
KimYutan <70746658+yutan0565@users.noreply.github.com>
2023-01-19 21:13:05 +0900

    Update 23289.py

--- SCT/First/10_기출/23289.py
+++ SCT/First/10_기출/23289.py
@@ -4,21 +4,23 @@ from collections import deque
 
 dx = [0, 0, -1, 1]
 dy = [1, -1, 0, 0]
+back_move = [-1,1,1,-1]
 
 def make_wind(x,y,direct):
     q = deque()
-    q.append([x,y, 5])
     visited = [[0 for _ in range(m)] for _ in range(n)]
-    flag_first = 0
+
+    s_x = x + dx[direct]
+    s_y = y + dy[direct]
+    temper[s_x][s_y] += 5
+    visited[s_x][s_y] = 1
+    q.append([s_x,s_y, 4])
     while q:
         for _ in range(len(q)):
             a,b,temp = q.popleft()
-            if flag_first == 0:
-                wind_direct = [0]
-                flag_first = 1
-            else:
-                wind_direct = [-1,0,1]
-            for i in wind_direct:
+            if temp == 0:
+                continue
+            for i in [-1,0,1]:
                 if direct == 0 or direct == 1:
                     nx = a + i
                     ny = b + dy[direct]
@@ -28,49 +30,49 @@ def make_wind(x,y,direct):
                 if 0<=nx<n and 0<=ny<m:
                     if visited[nx][ny] == 0:
                         if i == 0:
-                            if [[a,b],[nx,ny]] not in wall_list :
-                                if temp != 0:
-                                    q.append([nx,ny, temp - 1])
-                                    visited[nx][ny] = 1
-                                    temper[nx][ny] += temp
+                            if direct not in wall[a][b]:
+                                q.append([nx,ny, temp - 1])
+                                visited[nx][ny] = 1
+                                temper[nx][ny] += temp
                         else :
                             if direct == 0 or direct == 1:
-                                if [[a, b], [nx, ny-dy[direct]]] not in wall_list and [[nx,ny-dy[direct]], [nx, ny]] not in wall_list:
-                                    if temp != 0:
+                                if direct not in wall[nx][ny+back_move[direct]]:
+                                    if i == -1:
+                                        wind_direct = 2
+                                    else:
+                                        wind_direct = 3
+                                    if wind_direct not in wall[nx][ny+back_move[direct]]:
                                         q.append([nx, ny, temp - 1])
                                         visited[nx][ny] = 1
                                         temper[nx][ny] += temp
                             elif direct == 2 or direct == 3:
-                                if [[a, b], [ny-dx[direct], ny]] not in wall_list and [[nx-dx[direct], ny], [nx, ny]] not in wall_list:
-                                    if temp != 0:
+                                if direct not in wall[nx+back_move[direct]][ny]:
+                                    if i == -1:
+                                        wind_direct = 0
+                                    else:
+                                        wind_direct = 1
+                                    if wind_direct not in wall[nx+back_move[direct]][ny]:
                                         q.append([nx, ny, temp - 1])
                                         visited[nx][ny] = 1
                                         temper[nx][ny] += temp
 
-# 온도 조절
+# 온도
 def turn_temper():
     temp_temper = copy.deepcopy(temper)
-    turn_list = []
     for i in range(n):
         for j in range(m):
-            for d in range(4):
-                nx = i + dx[d]
-                ny = j + dy[d]
-                if 0<=nx<n and 0<=ny<m:
-                    if [[i,j],[nx,ny]] not in turn_list:
-                        chai = temper[i][j] - temper[nx][ny]
-                        if chai >= 4:
-                            temp_temper[i][j] -= chai//4
-                            temp_temper[nx][ny] += chai // 4
-                            turn_list.append([[nx,ny], [i,j]])
+            if temper[i][j] != 0:
+                for d in range(4):
+                    nx = i + dx[d]
+                    ny = j + dy[d]
+                    if 0<=nx<n and 0<=ny<m:
+                        if d not in wall[i][j]:
+                            chai = temper[i][j] - temper[nx][ny]
+                            if chai >= 4:
+                                temp_temper[i][j] -= chai//4
+                                temp_temper[nx][ny] += chai // 4
+
     return temp_temper
-
-def make_big_size_temper():
-    global temper
-    temper = [[0 for _ in range(m+2)]] + temper + [[0 for _ in range(m+2)]]
-    for i in range(1, n+1):
-        temper[i] = [0] + temper[i] + [0]
-
 
 def down_temper():
     for i in range(n):
@@ -79,42 +81,13 @@ def down_temper():
                 if temper[i][j] != 0:
                     temper[i][j] -= 1
     return temper
-    # global temper
-    # make_big_size_temper()
-    # temp_big_temper = copy.deepcopy(temper)
-    # q = deque()
-    # q.append([0,0])
-    # visited = [[0 for _ in range(m+2)] for _ in range(n+2)]
-    # visited[0][0] = 1
-    #
-    # while q:
-    #     a,b = q.popleft()
-    #     for i in range(4):
-    #         nx = a + dx[i]
-    #         ny = b + dy[i]
-    #         if 0 <= nx < n+2 and 0 <= ny < m+2:
-    #             if visited[nx][ny] == 0:
-    #                 if temper[nx][ny] == 0:
-    #                     q.append([nx,ny])
-    #                     visited[nx][ny] = 1
-    #                 else:
-    #                     print([nx,ny])
-    #                     temp_big_temper[nx][ny] -= 1
-    #                     visited[nx][ny] = 1
-    # print("늘림")
-    # for t in temp_big_temper:
-    #     print(t)
-    # out_temper = []
-    # for i in range(1,n+1):
-    #     out_temper.append(temp_big_temper[i][1:m+1])
-    # return out_temper
 
 
 def check_temper():
-    for machine in dict_machine.keys():
-        if dict_machine[machine][2] == 5:
-            x = dict_machine[machine][0]
-            y = dict_machine[machine][1]
+    for machine in dict_machine.values():
+        if machine[2] == 5:
+            x = machine[0]
+            y = machine[1]
             if temper[x][y] < k:
                 return -1
     return 1
@@ -132,18 +105,16 @@ for i in range(n):
 temper = [[0 for _ in range(m)] for _ in range(n)]
 
 w = int(sys.stdin.readline().rstrip())
-wall_list = []
+wall = [[[] for _ in range(m)] for _ in range(n)]
 for _ in range(w):
     x,y,t = map(int, sys.stdin.readline().rstrip().split())
     x,y = x-1,y-1
-    if t == 0:
-        if 0<=x-1<n and 0<=y<m:
-            wall_list.append([[x-1,y],[x,y]])
-            wall_list.append([[x, y], [x-1, y]])
-    elif t == 1:
-        if 0 <= x < n and 0 <= y + 1  < m:
-            wall_list.append([[x,y+1],[x,y]])
-            wall_list.append([[x, y], [x, y+1]])
+    if t == 1:
+            wall[x][y].append(0)    #우
+            wall[x][y+1].append(1)  #좌
+    elif t == 0:
+            wall[x][y].append(2)    #위
+            wall[x-1][y].append(3)  #아래
 
 result = 0
 
@@ -151,35 +122,31 @@ def show_temp():
     for t in temper:
         print(t)
 
-print(wall_list)
 while 1:
-    print(result)
-
-    print("처음")
-    show_temp()
-
     # 온풍기 바람이 나옴
     for machine in dict_machine.keys():
         if dict_machine[machine][2] != 5:
             now_direct = dict_machine[machine][2] - 1
             make_wind(dict_machine[machine][0],dict_machine[machine][1],now_direct)
-
-            print("바람")
-            show_temp()
-
     # 온도 조절
     temper = turn_temper()
 
-    print("조절")
-    show_temp()
-
     # 온도가 1 이상인 가장 바깥쪽 온도가 1 씩 감소
     temper = down_temper()
-
-    print("온도 내림")
     show_temp()
-
+    print()
     result += 1
+    if result  == 101:
+        break
     if check_temper() == 1:
         break
+
 print(result)
+
+"""
+2 2 1
+0 0
+1 5
+0
+
+"""
