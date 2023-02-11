17a264c827647e9317d666e1019064f601c34e3a
KimYutan <70746658+yutan0565@users.noreply.github.com>
2023-02-11 21:13:03 +0900

    Update 7569.py

--- SCT/First/05_코딩테스트 알고리즘 정복 3단계 BFS_DFS (난이도 중)/7569.py
+++ SCT/First/05_코딩테스트 알고리즘 정복 3단계 BFS_DFS (난이도 중)/7569.py
@@ -11,20 +11,21 @@ def bfs():
     dx = [1,-1,0,0,0,0]
     dy = [0,0,1,-1,0,0]
     dz = [0,0,0,0,1,-1]
-
     while q:
-        a,b,c = q.popleft()
-        for i in range(6):
-            nx = a + dx[i]
-            ny = b + dy[i]
-            nz = c + dz[i]
-            if 0<=nx<h and 0<=ny<n and 0<=nz<m:
-                if graph[nx][ny][nz] == 0:
-                    if visited[nx][ny][nz] == -1:
-                        q.append([nx,ny,nz])
-                        graph[nx][ny][nz] = 1
-                        visited[nx][ny][nz] = visited[a][b][c] + 1
-                        max_ = max(max_, visited[nx][ny][nz])
+        for _ in range(len(q)):
+            a,b,c = q.popleft()
+            for i in range(6):
+                nx = a + dx[i]
+                ny = b + dy[i]
+                nz = c + dz[i]
+                if 0<=nx<h and 0<=ny<n and 0<=nz<m:
+                    if graph[nx][ny][nz] == 0:
+                        if visited[nx][ny][nz] == -1:
+                            q.append([nx,ny,nz])
+                            graph[nx][ny][nz] = 1
+                            visited[nx][ny][nz] = visited[a][b][c] + 1
+                            max_ = max(max_, visited[nx][ny][nz])
+
     for i in range(h):
         for j in range(n):
             for k in range(m):
@@ -36,8 +37,7 @@ def bfs():
 m,n,h = map(int , sys.stdin.readline().rstrip().split())
 
 graph = [ [list(map(int, sys.stdin.readline().rstrip().split()))  for _ in range(n) ] for _ in range(h) ]
-visited = [ [[-1]*m  for _ in range(n) ] for _ in range(h) ]
-
+visited = [ [[-1 for _ in range(m)]  for _ in range(n) ] for _ in range(h) ]
 start = []
 for i in range(h):
     for j in range(n):
