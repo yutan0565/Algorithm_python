3155f6a5192c25da069c8e3c85040fe6fc341b98
KimYutan <70746658+yutan0565@users.noreply.github.com>
2023-02-14 00:01:11 +0900

    Update 5567.py

--- SCT/First/03_코딩테스트 알고리즘 정복 3단계 BFS_DFS (난이도 하)/5567.py
+++ SCT/First/03_코딩테스트 알고리즘 정복 3단계 BFS_DFS (난이도 하)/5567.py
@@ -28,3 +28,53 @@ for _ in range(m):
 
 result = bfs(1)
 print(result)
+
+
+
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+'''
+아이디어
+
+start와 거리가 1인 것과 거리가 2인 노드들의 개수를 파악
+
+visited[x, y] : x = 방문 여부, y = 1과의 거리
+'''
+
+n = int(input().rstrip())
+m = int(input().rstrip())
+graph = [[] for i in range(n + 1)]
+visited = [-1 for i in range(n + 1)]
+
+for i in range(m):
+    u, v = map(int, input().rstrip().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+def bfs(start):
+    queue = deque()
+    queue.append(start)
+    visited[start] = 0
+    count = 0
+    now_len = 0
+    while queue:
+        for _ in range(len(queue)):
+            node = queue.popleft()
+            if visited[node] == 1 or visited[node] == 2:
+                count += 1
+            for u in graph[node]:
+                if visited[u] == -1:
+                    visited[u] = visited[node] + 1
+                    queue.append(u)
+        now_len += 1
+        if now_len == 2:
+            break
+    return count
+
+start = 1
+result = bfs(start)
+print(result)
+
