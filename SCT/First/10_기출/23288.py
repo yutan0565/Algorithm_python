ae822b562b513617a2b6baf41fa7f31f8e55eeb0
KimYutan <70746658+yutan0565@users.noreply.github.com>
2023-02-21 21:05:29 +0900

    Update 23288.py

--- SCT/First/10_기출/23288.py
+++ SCT/First/10_기출/23288.py
@@ -1,31 +1,54 @@
 import sys
 from collections import deque
 
+def rebuild_dice_map(direct):
+    global  dice_map_col, dice_map_row
+    if direct == 1: #위
+        temp = dice_map_col[-1]
+        dice_map_col = [temp] + dice_map_col
+        dice_map_col = dice_map_col[:-1]
+        dice_map_row[1] = dice_map_col[1]
+    elif direct == 3: #아래
+        temp = dice_map_col[0]
+        dice_map_col = dice_map_col + [temp]
+        dice_map_col = dice_map_col[1:]
+        dice_map_row[1] = dice_map_col[1]
+    elif direct == 0:
+        new_dice_map_col = [dice_map_col[0], dice_map_row[0], dice_map_col[2],dice_map_row[2]]
+        new_dice_map_row = [dice_map_col[-1], dice_map_row[0], dice_map_row[1] ]
+        dice_map_col = new_dice_map_col
+        dice_map_row = new_dice_map_row
+    elif direct == 2:
+        new_dice_map_col = [dice_map_col[0], dice_map_row[2], dice_map_col[2],dice_map_row[0]]
+        new_dice_map_row = [dice_map_row[1], dice_map_row[2], dice_map_col[-1] ]
+        dice_map_col = new_dice_map_col
+        dice_map_row = new_dice_map_row
+
 def roll_dice():
     global  now_direct, now_under, now_x, now_y, result
-
     nx = now_x + dx[now_direct]
     ny = now_y + dy[now_direct]
     if not(0<=nx<n and 0<=ny<m):
-        print("Sss")
         now_direct = (now_direct + 2)%4
         nx = now_x + dx[now_direct]
         ny = now_y + dy[now_direct]
+    rebuild_dice_map(now_direct)
 
     # 수사위 밑 업데이트
-    now_under = dice_dict[now_under][now_direct]
+    now_under = dice_map_col[-1]
     graph_number = graph[nx][ny]
     # 굴러간 곳에서 첨수 취득
     count = cal_score(nx, ny, graph_number)
     result += count*graph_number
 
-    print(now_under, graph_number)
     if now_under > graph_number:
         now_direct = (now_direct + 1)%4
     elif now_under< graph_number:
         now_direct = (now_direct - 1) % 4
     elif now_under == graph_number:
         pass
+
+
 
     now_x = nx
     now_y = ny
@@ -52,8 +75,10 @@ def cal_score(x, y, number):
                         visited[nx][ny] = 1
                         count += 1
     return count
-dice_dict = {1:[3,2,4,5], 2:[3,1,4,6], 3:[6,5,1,2],
-             4:[1,5,6,2], 5:[3,6,4,1], 6:[3,2,4,5]}
+
+dice_map_row = [4,1,3]
+dice_map_col = [2,1,5,6]
+
 
 dx = [0,1,0,-1]
 dy = [1,0,-1,0]
@@ -72,7 +97,6 @@ roll_count = 0
 while 1:
     number = roll_dice()
     roll_count += 1
-    print(roll_count)
     if roll_count == k:
         break
 print(result)
