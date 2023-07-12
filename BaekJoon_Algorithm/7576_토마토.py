fedd046e158106af2ff141c4bd88e66b943a144d
KimYutan <70746658+yutan0565@users.noreply.github.com>
2023-07-12 21:15:12 +0900

    Update 7576_토마토.py

--- BaekJoon_Algorithm/7576_토마토.py
+++ BaekJoon_Algorithm/7576_토마토.py
@@ -20,27 +20,31 @@ def bfs(temp_list):
     dx = [0,1,0,-1]
     dy = [1,0,-1,0]
 
+    result = 0
     while q:
-        a, b = q.popleft()
-        for i in range(len(dx)):
-            nx = a + dx[i]
-            ny = b + dy[i]
-            if nx >=0 and ny >=0 and nx < N and ny < M:
-                if visited[nx][ny] != True:
-                    if graph[nx][ny] == 0:
-                        q.append([nx, ny])
-                        visited[nx][ny] = True
-                        graph[nx][ny] = graph[a][b] + 1
+        result += 1
+        print(result, len(q))
+        for i in range(len(q)):
+            a, b = q.popleft()
+            for i in range(len(dx)):
+                nx = a + dx[i]
+                ny = b + dy[i]
+                if nx >=0 and ny >=0 and nx < N and ny < M:
+                    if visited[nx][ny] != True:
+                        if graph[nx][ny] == 0:
+                            q.append([nx, ny])
+                            visited[nx][ny] = True
+                            graph[nx][ny] = graph[a][b] + 1
 
     temp_max = -1
     for i in range(N):
         for j in range(M):
-            if temp_max < graph[i][j]:
-                temp_max = graph[i][j]
+            # if temp_max < graph[i][j]:
+            #     temp_max = graph[i][j]
             if graph[i][j] == 0:
                 return 0
 
-    return temp_max
+    return result
 
 
 temp_list = []
