a44e297bbbc455ed8d10e156b28bcbd09a9cbd24
KimYutan <70746658+yutan0565@users.noreply.github.com>
2023-09-09 22:49:59 +0900

    Update 코드트리 채점기.py

--- SCT/04/10_기출/코드트리 채점기.py
+++ SCT/04/10_기출/코드트리 채점기.py
@@ -2,101 +2,88 @@ import sys, copy
 import heapq
 from collections import defaultdict
 
+
 def make_machine(n, u0):
-    for num in range(1,n+1):
-        dict_machine_domain[num] = ""
-        dict_machine_url[num] = ""
+    for num in range(1, n + 1):
+        dict_machine_domain[num] = None
         heapq.heappush(avail_machine_q, num)
 
     domain, id = u0.split("/")
     time = 0
     pri = 1
-    dict_waiting_q[domain] = []
-    dict_waiting_q[domain].append([pri, time, u0])
+    if not (dict_waiting_q.get(domain)):
+        dict_waiting_q[domain] = []
+    heapq.heappush(dict_waiting_q[domain], [pri, time, u0])
+    use_url.add(u0)
+
 
 def start_check(t, p, u):
     domain, id = u.split("/")
     if u not in use_url:
-        use_url.append(u)
-        if not(dict_waiting_q.get(domain)):
+        use_url.add(u)
+        if not (dict_waiting_q.get(domain)):
             dict_waiting_q[domain] = []
-        dict_waiting_q[domain].append([p, t, u])
+        heapq.heappush(dict_waiting_q[domain], [p, t, u])
 
 
 def try_check(t):
-    if len(avail_machine_q) == 0:
+    # 사용 가능한 기계가 없는 경우 / 대기큐에 아무것도 없는 경우 (use_url에 없는경우)
+    if len(avail_machine_q) == 0 or len(use_url) == 0:
         return
+
+    min_wait = [float("inf"), float("inf"), None, None]
 
     for now_domain in dict_waiting_q.keys():
         if dict_waiting_q[now_domain] == []:
             continue
+        # 지금 대기 domain별 대기줄에 있는 것중 전번째
         now_p, now_t, now_u = dict_waiting_q[now_domain][0]
         if now_domain in use_domain:
-            print(t)
-            print("이미 진행중인 domain : ", now_domain)
-            print(use_domain)
             continue
-        if history_q != []:
-            continue_flag = 0
-            new_history_q = copy.deepcopy(history_q)
-            for i in range(len(history_q)):
-                pri,his_start, his_end, his_u, his_domain =min(new_history_q)
-                print("?")
-                if now_domain == his_domain:
-                    gap = his_end - his_start
-                    print("gap : ", gap)
-                    if t < his_start + 3 * gap:
-                        continue_flag = 1
-                        print("조건에 맞지 않는 domain: ", now_domain)
-                        break
-                new_history_q.remove([pri,his_start, his_end, his_u, his_domain])
-            if continue_flag == 1:
-                continue
-        machine_num = heapq.heappop(avail_machine_q)
-        dict_machine_domain[machine_num] = now_domain
-        dict_machine_url[machine_num] = now_domain
-        use_domain.append(now_domain)
-        dict_waiting_q[now_domain].remove([ now_p, now_t, now_u])
-        judging_q.append([t, now_u, now_domain])
-        print("시작")
+        if not (dict_domain_time.get(now_domain)):
+            dict_domain_time[now_domain] = [0, 0]
+        start, gap = dict_domain_time[now_domain]
+        if t < start + 3 * gap:
+            continue
+        min_wait = min(min_wait, [now_p, now_t, now_domain, now_u])
+
+    # min_wait으로 선정된게 아무것도 없는 경우
+    if min_wait[2] == None:
+        return
+    try_p, try_t, try_domain, try_u = min_wait
+    machine_num = heapq.heappop(avail_machine_q)
+    dict_machine_domain[machine_num] = try_domain
+    use_domain.add(try_domain)
+    use_url.remove(try_u)
+    # root가 최소값인것은 맞음 / 하지만 heappop을 사용해야지 재 정렬이 됨
+    heapq.heappop(dict_waiting_q[try_domain])
+   # del dict_waiting_q[try_domain][0] # 정렬이 안됨
+    dict_domain_time[try_domain][0] = t
 
 def end_check(t, j_id):
-    if dict_machine_url[j_id] == "":
+    if dict_machine_domain[j_id] == None:
         return
     heapq.heappush(avail_machine_q, j_id)
-    now_u = dict_machine_url[j_id]
-    dict_machine_domain[j_id] = ""
-    dict_machine_url[j_id] = ""
-    for i in range(len(judging_q)):
-        jud_t, jud_u, jud_domain = judging_q[i]
-        if now_u == jud_u:
-            judging_q.remove([jud_t, jud_u, jud_domain])
-            use_domain.remove(jud_domain)
-            use_url.remove(jud_u)
-            history_q.append([-t, jud_t, t, jud_u, jud_domain])
-            break
+    now_domain = dict_machine_domain[j_id]
+    dict_machine_domain[j_id] = None
+    dict_domain_time[now_domain][1] = t - dict_domain_time[now_domain][0]
+    use_domain.remove(now_domain)
 
 
-def waiting_q_info(t):
-    count = 0
-    for domain in dict_waiting_q.keys():
-        count += len(dict_waiting_q[domain])
-    return count
+def waiting_q_info():
+    return len(use_url)
 
 Q = int(sys.stdin.readline().rstrip())
-
 
 avail_machine_q = []
 
 dict_machine_domain = {}
-dict_machine_url = {}
-use_domain = []
-use_url = []
+dict_domain_time = {}
+
+use_domain = set()
+use_url = set()
 
 dict_waiting_q = {}
-judging_q = []
-history_q = []
-
 result_list = []
 
 for round in range(1, Q + 1):
@@ -124,13 +111,20 @@ for round in range(1, Q + 1):
 
     elif order == 500:
         t = int(order_info[1])
-        result = waiting_q_info(t)
+        result = waiting_q_info()
         result_list.append(result)
+
+    # print("====================")
+    # print("order : ", order)
+    # print("기계 도메인 : ",dict_machine_domain)
+    # print("기계 시간 : ",dict_domain_time)
+    # print("대기 : ",dict_waiting_q)
+    # print("사용 domain : ", use_domain)
+    # print("사용 url : ", use_url)
+    # print()
 
 for r in result_list:
     print(r)
-
-
 
 """
 19
